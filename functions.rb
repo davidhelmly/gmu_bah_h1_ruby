cb58f211a4715d061ce39760a3128943be771837
davidhelmly <david.j.helmly@gmail.com>
2014-10-09 16:36:04 -0400

    homework 1

--- functions.rb
+++ functions.rb
@@ -1,13 +1,23 @@
 # Return a reversed copy of the array
 def reverse(an_array)
-	#write your code here
+	an_array.reverse
 end
 
-# Return a map o fletters and counts of letters
+# Return a map of letters and counts of letters
 # Letters should be lowercase before counting
 # For example, histogram('hello') returns {'h'=>1, 'l'=>2, 'o'=>1}
 def histogram(a_string)
-	#write your code here
+	count = 0
+	letter_map = {}
+	while count < a_string.length
+		if letter_map[a_string[count]] == nil
+			letter_map[a_string[count]] = 1
+		else
+			letter_map[a_string[count]] += 1
+		end
+		count += 1
+	end
+	return letter_map
 end
 
 # Sum all the numbers in the array
@@ -15,7 +25,16 @@ end
 # Be sure to only sum the numbers
 # (Hint: the is_a? method might be useful here)
 def sum_only_numbers(an_array)
-	#write your code here
+	summation = 0
+	for item in an_array
+		if item.is_a? Integer
+			summation += item
+		end
+		if item.is_a? Float
+			summation += item
+		end
+	end
+	return summation
 end
 
 # For i from 1 to 100, return an array.
@@ -26,11 +45,27 @@ end
 # Otherwise, the element is simply the value of i
 # For example [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', ..., 14, 'FizzBuzz', ...]
 def fizzbuzz
-	#write your code here
+	count = 0
+	an_array = []
+	while count <100
+		an_array[count] = count
+		if count%5 == 0
+			an_array[count] = 'Buzz'
+		end
+		if count%3 == 0
+			if count%5 == 0
+				an_array[count] = 'FizzBuzz'
+			else
+				an_array[count] = 'Fizz'
+			end
+		end
+		count +=1
+	end
+	return an_array
 end
 
 # Uncomment each of these to test your functions
-# puts revers([3,6,'dog']).inspect
+# puts reverse([3,6,'dog']).inspect
 # puts histogram('The Quick brown fox').inspect
 # puts sum_only_numbers [4, 'foo', [ ], 27, :rain, 3.14]
 # puts fizzbuzz.join("\n")
